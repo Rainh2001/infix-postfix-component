c8eb43d73f2c0bdd8a457436f65e78fb766645da
Rain Holloway <rainh2001@gmail.com>
2021-07-15 14:36:09 +1000

    Implemented algorithm to check for constants that are longer than 1 character. Changed infix to an array to allow this to work

--- src/InfixToPostfix.js
+++ src/InfixToPostfix.js
@@ -94,45 +94,109 @@ function InfixToPostfix() {
                 <input ref={equationInput} placeholder="Enter Infix Equation`..." type="text"/>
                 <button
                 onClick={() => {
-                    const equation = equationInput.current.value.replaceAll(" ", "");
-                    setInfix(equation);
+                    var equation = equationInput.current.value.replaceAll(" ", "");
+
+                    const isNum = (char) => {
+                        return (char >= '0' && char <= '9');
+                    }
+
+                    const isAlpha = (char) => {
+                        return (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z');
+                    }
+
+                    const isOperator = (char) => {
+                        let operators = "+-/*^()".split("");
+                        return operators.indexOf(char) > -1;
+                    }
+
+                    const isValid = (char) => {
+                        return isNum(char) || isAlpha(char) || isOperator(char);
+                    }
+
+                    // Separate constants from variables
+                    for(let i = 1; i < equation.length; i++){
+                        let [c1, c2] = [equation[i-1], equation[i]];
+
+                        // If c1 is a number and c2 is a letter or vice versa. Also if both are Alpha
+                        if((isNum(c1) && isAlpha(c2)) || (isNum(c2) && isAlpha(c1)) || (isAlpha(c1) && isAlpha(c2))){
+                            equation = equation.slice(0, i) + '*' + equation.slice(i);
+                        }
+                    }
+
+                    // Convert string into array of characters, however numbers that are greater than 9 will be longer than 1 character
+                    let arr = [];
+                    let pos = 0;
+                    let currentNum = "";
+
+                    const pushNum = () => {
+                        if(currentNum[currentNum.length-1] === "."){
+                            currentNum = currentNum.slice(0, currentNum.length-1);
+                        }
+                        arr.push(currentNum);
+                        currentNum = "";
+                    }
+
+                    while(pos < equation.length){
+                        let char = equation[pos];
+                        if(isAlpha(char) || isOperator(char)){
+
+                            if(currentNum.length > 0){
+                                pushNum();
+                            }
+
+                            arr.push(char);
+                        } else if(isNum(char) || char === '.'){
+                            currentNum += char;
+                            if(isNaN(currentNum)) return;
+                        } else return;
+
+                        pos++;
+                    }
+
+                    if(currentNum.length > 0){
+                        pushNum();
+                    }
+
+                    setInfix(arr);
                     setHistoryCount(0);
-                    calculateResult(equation);
+                    calculateResult(arr);
                 }}
                 >Convert To Postfix</button>
             </div>
-            <div
-            style={{
-                marginTop: '1rem'
-            }}
-            >
-                <button
-                onClick={() => {
-                    setHistoryCount(current => {
-                        if(current === 0) return current;
-                        return current - 1;
-                    });
+            { infix &&
+                <div
+                style={{
+                    marginTop: '1rem'
                 }}
-                >Previous</button>
-                <button
-                onClick={() => {
-                    setHistoryCount(current => {
-                        if(current === history.length-1) return current;
-                        return current + 1;
-                    });
-                }}
-                >Next</button>
-                <span
-                style={{ marginLeft: '1rem' }}
                 >
                     <button
-                    onClick={() => setHistoryCount(0)}
-                    >Start Again</button>
+                    onClick={() => {
+                        setHistoryCount(current => {
+                            if(current === 0) return current;
+                            return current - 1;
+                        });
+                    }}
+                    >Previous</button>
                     <button
-                    onClick={() => setHistoryCount(history.length-1)}
-                    >Finish</button>
-                </span>
-            </div>
+                    onClick={() => {
+                        setHistoryCount(current => {
+                            if(current === history.length-1) return current;
+                            return current + 1;
+                        });
+                    }}
+                    >Next</button>
+                    <span
+                    style={{ marginLeft: '1rem' }}
+                    >
+                        <button
+                        onClick={() => setHistoryCount(0)}
+                        >Start Again</button>
+                        <button
+                        onClick={() => setHistoryCount(history.length-1)}
+                        >Finish</button>
+                    </span>
+                </div>
+            }
             <div
             style={{
                 marginTop: '1rem'
@@ -158,15 +222,7 @@ function InfixToPostfix() {
                         id="infix-stack"
                         >
                             { 
-                                // history[historyCount].infixStr.split("").map((char, i) => {
-                                //     return <div
-                                //     key={`${char}${i}`}
-                                //     className="stack-box infix-stack-box"
-                                //     >
-                                //         <span>{ char }</span>
-                                //     </div>
-                                // })
-                                infix.split("").map((char, i) => {
+                                infix.map((char, i) => {
                                     let highlighted = history[historyCount].infixIndex === i;
                                     return <div
                                     key={`${char}${i}`}
